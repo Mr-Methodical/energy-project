a632a808cf7340b3f07ad87c0cb42452ce5a72cb
Ryan Howe <r3howe@uwaterloo.ca>
2026-04-21 05:55:00 -0700

    Connecting Postgres database

--- src/tcl/ingest.tcl
+++ src/tcl/ingest.tcl
@@ -10,10 +10,13 @@ set tableName "owid_energy"
 set url "https://raw.githubusercontent.com/owid/energy-data/master/owid-energy-data.csv"
 set filename "/tmp/owid-energy-data.csv"
 
-# Downloading the CSV
-puts "Downloading OWID dataset from GitHub"
+puts "Downloading CSV: OWID dataset from GitHub"
 exec curl -sL -o $filename $url
 puts "Download complete"
 
 puts "Connecting to database $dbName"
+tdbc::postgres::connection create db -user $dbUser -password $dbPass -db $dbName
+puts "Connected!"
+
+puts "Reading CSV header"
 
