f9f7fee779e78e7e80c1a08cf91c298a5a61a963
Ryan Howe <r3howe@uwaterloo.ca>
2026-04-21 12:06:43 -0700

    added libraries and constants

--- src/java/com/example/EnergyServlet.java
+++ src/java/com/example/EnergyServlet.java
@@ -1,1 +1,12 @@
 package com.example
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.*;
+import java.sql.*;
+
+public class EnergyServlet extends HttpServlet {
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/energydb";
+    private static final String DB_USER = "energyuser";
+    private static final String DB_PASS = "energy123";
+}
